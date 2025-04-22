7ad295cfc5c2fad42c572f12ac391dd79a3efa19
JamieDuty <jamiedennisduty@gmail.com>
2025-04-22 13:57:20 -0500

    Update ACME_retail_system.py

--- ACME_retail_system.py
+++ ACME_retail_system.py
@@ -1,2 +1,16 @@
 #ACME_retail_system
 #runs from chapter 10 team project.py
+def main():
+    #main will give the user the option to access the invetory system or retail system
+    #it will then lead into the options in the inventory menu or the retail menu
+    print("Please choose from the options below: ")
+    print("To access the inventory control system, press 1.")
+    print("To access the retail store, press 2.")
+    print('')
+    menu_choice = int(input("Enter your choice: "))
+    #input handling here -------
+    if menu_choice == 1:
+        #run the inventory control menu
+
+
+main()
