fdfef252bc5b7d6f2538d2b26fcd68330f29c22b
JamieDuty <jamiedennisduty@gmail.com>
2025-04-22 13:49:48 -0500

    Update Chapter 10 Team Project.py

--- Chapter 10 Team Project.py
+++ Chapter 10 Team Project.py
@@ -1,6 +1,14 @@
 #Chapter 10 Team Project
-
-
 class RetailItem:      #Done by Jamie
-    
-    
+    def __init__(self, description, units, price):
+        self.__description = description
+        self.__units = units
+        self.__price = price
+    #####-----getter methods-----#####
+    #all should be strings
+    def get_description(self):
+        return self.__description
+    def get_units(self):
+        return self.__units
+    def get_price(self): 
+        return self.__price
