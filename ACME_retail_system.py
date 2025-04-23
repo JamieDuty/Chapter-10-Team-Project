56558abfa2ce5b4d939609ea54fa61d3513de4c2
JamieDuty <jamiedennisduty@gmail.com>
2025-04-23 13:45:54 -0500

    Update ACME_retail_system.py

--- ACME_retail_system.py
+++ ACME_retail_system.py
@@ -2,7 +2,6 @@ import Chapter10TeamProject as file
 #ACME_retail_system
 #runs from chapter 10 team project.py
 def main():
-    password = 'Password'
     #main will give the user the option to access the invetory system or retail system
     #it will then lead into the options in the inventory menu or the retail menu
     print("Please choose from the options below: ")
@@ -14,23 +13,48 @@ def main():
     while menu_choice != '1' and menu_choice != '2':
         menu_choice = input("Enter 1 or 2 only: ")
     if menu_choice == '1':
-        #run the inventory control menu
-        #needs to search for inventory file, if not output creating
-        user_password = input("Enter the inventory control password: ")
-        while user_password != password:
-            print('Incorrect!')
-            user_password = input('Enter the inventory control password: ')
-        if user_password == password:
-            print('Correct!')
-            print('\nWelcome to the ACME inventory control system')
-            print('\nPlease select an action from the following:')
+        inventory_menu()
     if menu_choice == '2':
         while 1:
             print("1 - View cart\n2 - Display items for sale\n3- Purchase item\n4 - Empty cart and start over\n5-Check out\n6 - Exit")
             choice2 = input("Please enter a selection: ")
-        #run the retail menu
-     
-        
+def inventory_menu():
+    password = 'Password'
+    #run the inventory control menu
+    #needs to search for inventory file, if not output creating
+    user_password = input("Enter the inventory control password: ")
+    while user_password != password:
+        print('Incorrect!')
+        user_password = input('Enter the inventory control password: ')
+    if user_password == password:
+        print('Correct!')
+        print('\nWelcome to the ACME inventory control system')
+        print('\nPlease select an action from the following:')
+        print('\nPress 1 to display the current inventory.')
+        print('\nPress 2 to add inventory items to the current inventory.')
+        print('\nPress 3 to save the inventory.')
+        print('\nPress 4 to exit')
+        inventory_choice = input('Select an action (1, 2, or 3. Press 4 to EXIT): ')
+        #needs handling
+def call_inventory_choice():
+    inventory_choice = inventory_menu()
+    while choice != 4:
+        if inventory_choice == 1:
+            display_inventory()
+        elif inventory_choice == 2:
+            display_inventory()
+        elif inventory_choice == 3:
+            display_inventory()
+#Inventory Choices
+def display_inventory():
+    pass
+def add_to_inventory():
+    pass
+def write_inventory_data():
+    pass
+def end():
+    pass        
+#Retail Choices
 def display_cart():
     pass
 def display_cart():
