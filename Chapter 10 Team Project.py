5f666098361ee5d0c2dd659f45da0c05d8ff2e5f
NoahBraxton <jackerman881@gmail.com>
2025-04-23 13:03:58 -0500

    Update Chapter 10 Team Project.py

--- Chapter 10 Team Project.py
+++ Chapter 10 Team Project.py
@@ -12,3 +12,21 @@ class RetailItem:      #Done by Jamie
         return self.__units
     def get_price(self): 
         return self.__price
+
+class CashRegister:
+    def __init__(self):
+        self.items = []
+    def purchase_item(self, item):
+        self.items.append(item)
+    def get_total(self):
+        total = 0
+        for item in self.items:
+            total += item.get_price()
+        return total
+    def get_cart(self):
+        for item in self.items:
+            print("Description:", item.get_description())
+            print("Unit(s):", item.get_units())
+            print("Price: $", item.get_price(), end = "")
+    def empty():
+        self.items = []
