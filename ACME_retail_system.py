0c004ff50364fcfe79ac84ea3fe8b617bd68eea2
JamieDuty <jamiedennisduty@gmail.com>
2025-04-23 13:15:42 -0500

    Update ACME_retail_system.py

--- ACME_retail_system.py
+++ ACME_retail_system.py
@@ -1,6 +1,7 @@
 #ACME_retail_system
 #runs from chapter 10 team project.py
 def main():
+    password = 'Password'
     #main will give the user the option to access the invetory system or retail system
     #it will then lead into the options in the inventory menu or the retail menu
     print("Please choose from the options below: ")
@@ -9,11 +10,20 @@ def main():
     print('')
     menu_choice = int(input("Enter your choice: "))
     #input handling here -------
-    if menu_choice == 1:
+    while menu_choice != '1' and menu_choice != '2':
+        menu_choice = input("Enter 1 or 2 only: ")
+    if menu_choice == '1':
         #run the inventory control menu
-    if menu_choice == 2:
+        #needs to search for inventory file, if not output creating
+        user_password = input("Enter the inventory control password: ")
+        while user_password != password:
+            print('Incorrect!')
+            user_password = input('Enter the inventory control password: ')
+        if user_password == password:
+            print('Correct!')
+            #go to inventory control system
+    if menu_choice == '2':
+        print('2')
         #run the retail menu
-    else:
-        menu_choice = int(input("Please enter 1 or 2 only: "))
-
+    
 main()
